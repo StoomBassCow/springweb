21bc6cc359b97e832fe1edf05da32cb2a77f717e
cesar.huerta <cesar.huerta@DESKTOP-9J4HUUJ.ad.gpoeisei.net.mx>
2020-09-17 16:29:57 -0500

    Archivo appConfig establecido para las vistas y los assets

---
+++ src/main/java/com/mx/spring/config/appConfig.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mx.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.mx.spring"})
+
+public class appConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver resolver() {
+
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/views");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+
+    }
+
+    @Override
+    public void addResourceHandler(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("/aseets/**");
+    }
+
+}
